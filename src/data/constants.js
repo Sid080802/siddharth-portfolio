c85bb059039daff7be74efbfb23903c22f21587d
Siddharth Godambe <siddarthgodambe123@gmail.com>
2025-02-24 12:22:37 +0530

    add project into repo

---
+++ src/data/constants.js
@@ -0,0 +1,250 @@
+
+export const Bio = {
+  name: "Siddharth Bhimaji Godambe",
+  roles: [
+    "Full Stack Developer",
+    "Programmer",
+  ],
+  description:
+    "I'm a highly motivated and versatile individual with a strong aptitude for problem-solving and a deep commitment to continuous development. I enjoy tackling complex challenges, analyzing data, and developing effective solutions. I'm a firm believer in lifelong learning and actively pursue opportunities to expand my knowledge and skillset.",
+  github: "https://github.com/Sid080802",
+  gmail: "siddarthgodambe123@gmail.com",
+  resume:
+    "https://drive.google.com/file/d/1-VhnR6Hn-AcEIP5CIAaRKn2j-3Pgyxwx/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/siddharth-godambe-473b642b4/",
+
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      
+     
+      {
+        name: "HTML5",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS3",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+     
+   
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://www.vectorlogo.zone/logos/expressjs/expressjs-icon.svg"
+          },
+  
+          {
+            name: "Python",
+            image:
+              "https://www.svgrepo.com/show/452091/python.svg",
+          },
+   
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+   
+    ],
+  },
+
+  {
+    title: "Others",
+    skills: [
+      
+     
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Render",
+        image:
+          "https://i.pinimg.com/1200x/8b/7c/ac/8b7cac4d85303b79158dd3cf2d9b63c8.jpg",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name:"vercel",
+        image:"https://www.svgrepo.com/show/354513/vercel-icon.svg",
+
+      },
+
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "./logo.jfif",
+    role: "Software Developer Intern",
+    company: "Numetry Technologies",
+    date: "Jan 2025 - Present",
+    desc: `
+      • Developing and maintaining full-stack applications using the MERN stack.
+      • Working on building scalable, secure, and high-performance web applications.
+      • Collaborating with the development team to implement new features and optimize performance.
+      • Handling API integrations, database operations, and real-time functionalities.
+      • Contributed to the development and enhancement of the Numtery company website, improving functionality and user experience.
+    `,
+    skills: [
+      "Node.js",
+      "Express.js",
+      "ReactJS",
+      "MongoDB",
+      "HTML5",
+      "CSS3",
+      "JavaScript",
+      "API Integration",
+      "Bootstrap"
+    ],
+    doc: "https://drive.google.com/file/d/1fdw_O3Z3ceucoIz1h8ruwxlNrTnobFT4/view?usp=sharing",
+  },
+
+
+
+  {
+    id: 1,
+    img: "./cub2king.jfif",
+    role: "Software Engineer Intern",
+    company: "Cub To King IT Solutions Pvt. Ltd.",
+    date: "Jan 2024 - Nov 2024",
+    desc: `
+    •	Completed extensive training (4-6 months) on web development technologies, including HTML5, CSS3, CSS Flexbox, JavaScript, and Java.
+    •	Gained hands-on experience in form handling and implementing CRUD operations for dynamic applications.
+•	Contributed to real-time projects, enhancing team collaboration and software quality.
+
+    `,
+       skills: [
+      "HTML5",
+      "CSS3",
+      "Bootstrap",
+      "Java",
+      "JavaScript",
+      "MYSQL"
+    ],
+    doc: "https://drive.google.com/file/d/1Poxu5x_3vBLYn4gwes5no7vdIzzvZNT0/view?usp=sharing",
+  },
+
+];
+
+
+export const education = [
+  {
+    id: 0,
+    img: "./dy.jfif",
+    school: "Dr. D. Y. Patil Technical Campus",
+    date: "2023 - Present",
+    grade: " 8.50 SGPA",
+    desc: "", 
+    degree: "Master of Computer Application",
+  },
+
+  {
+    id: 1,
+    img: "./indrayani.jpeg",
+    school: "Indrayani Mahavidyalaya",
+    date: "2020 - 2023",
+    grade: " 7.64 CGPA",
+    desc: "", 
+    degree: "Bacholer's of Computer Application",
+  },
+];
+
+export const projects = [
+  {
+    id: 11,
+    title: "Shopify-Clone",
+    description:
+    `
+      The Shopify Clone is a full-featured e-commerce platform built using the MERN stack (MongoDB, Express.js, React.js, Node.js) with React Vite for a fast and efficient frontend experience. The platform replicates the core functionalities of Shopify, enabling users to browse, search, and purchase products seamlessly.
+
+ User Authentication: Secure login/signup functionality for customers using JWT authentication.
+ Product Listings: Display a variety of products with images, descriptions, prices, and categories.
+ Search & Filtering: Advanced search and filter options for easy product discovery.
+ Shopping Cart & Wishlist: Add products to the cart or wishlist, with real-time updates.
+ Checkout & Order Processing: Seamless checkout experience with a success confirmation page.
+ Responsive Design: Fully optimized for mobile and desktop users.
+ MongoDB Atlas: Cloud-based database storage for scalability and performance.`,
+    image:
+      "./shopify.png",
+    tags: [
+      "MongoDb",
+      "Express.Js",
+      "React.js",
+      "Node.Js",
+      "CSS3",
+      "React Vite"
+      
+    ],
+    category: "web app",
+    github: "https://github.com/Sid080802/shopify-clone.git",
+    webapp: "https://shopify-clone-snowy.vercel.app/",
+  },
+  
+  {
+    id: 9,
+    title: "WorkSync",
+    description:
+      " The Task Dashboard is a dynamic and intuitive task management system built using the MERN stack (MongoDB, Express.js, React.js, Node.js) with React Vite for a smooth and responsive frontend experience. This dashboard helps users efficiently create, track, and manage their tasks, ensuring better productivity and organization. ",
+      image:
+      "./Task.png",
+      tags: [
+        "MongoDb",
+        "Express.Js",
+        "React.js",
+        "Node.Js",
+        "CSS3",
+        "React Vite"
+        
+      ],
+    category: "web app",
+    github: "https://github.com/Sid080802/TaskDashboard-numetry.git",
+    webapp: "https://task-dashboard-numetry.vercel.app/",
+  },
+]
